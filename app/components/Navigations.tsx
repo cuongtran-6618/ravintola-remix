5013f99f0f9a174ce1ba39775ded5311238add9b
Cuong Tran <mailtocuong@gmail.com>
2023-03-05 21:05:12 +0200

    complete navigation mobile

--- app/components/Navigations.tsx
+++ app/components/Navigations.tsx
@@ -16,79 +16,81 @@ export default function Navigations() {
   };
 
   return (
-    <div className="container text-xl pt-8 pb-4 px-4 grid grid-flow-row">
-      <div className="logo-container">Ravintola</div>
-      <nav id="main-navigation">
-        <button className="md:hidden" onClick={toggleMobileNav}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
-            />
-          </svg>
-        </button>
-        <div
-          id="navigation-container-wrapper"
-          className={
-            isNavOpen
-              ? "fixed top-0 bottom-0 right-0 left-0 bg-white z-2 p-8"
-              : "hidden"
-          }
-        >
-          <div className="relative">
-            <button
-              className="absolute top-0 right-0 z-2"
-              onClick={closeMobileNav}
+    <div className="container text-xl py-8 px-4 flex justify-between items-center bg-tumma-puu align-center">
+      <div className="logo-container order-first text-white ">Ravintola</div>
+      <div className="order-last">
+        <nav id="main-navigation">
+          <button className="md:hidden" onClick={toggleMobileNav}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-white "
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+              />
+            </svg>
+          </button>
+          <div
+            id="navigation-container-wrapper"
+            className={
+              isNavOpen
+                ? "fixed top-0 bottom-0 right-0 left-0 bg-white z-2 p-8"
+                : "hidden"
+            }
+          >
+            <div className="relative">
+              <button
+                className="absolute top-0 right-0 z-2"
+                onClick={closeMobileNav}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-            <div id="navigation-container" className="pt-16">
-              <ul className="flex flex-col justify-center align-baseline md:flex-row gap-4 ">
-                <li className={navigationItemClass}>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li className={navigationItemClass}>
-                  <NavLink to="/menu">Menu</NavLink>
-                </li>
-                <li className={navigationItemClass}>
-                  <NavLink to="/our-story">Our story</NavLink>
-                </li>
-                <li className={navigationItemClass}>
-                  <NavLink to="/groups-and-events">Groups and events</NavLink>
-                </li>
-                <li className={navigationItemClass}>
-                  <NavLink to="/gallery">Gallery</NavLink>
-                </li>
-                <li className={navigationItemClass}>
-                  <NavLink to="/contact">Contact</NavLink>
-                </li>
-              </ul>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+              <div id="navigation-container" className="pt-16">
+                <ul className="flex flex-col justify-center align-baseline md:flex-row gap-4 ">
+                  <li className={navigationItemClass}>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+                  <li className={navigationItemClass}>
+                    <NavLink to="/menu">Menu</NavLink>
+                  </li>
+                  <li className={navigationItemClass}>
+                    <NavLink to="/our-story">Our story</NavLink>
+                  </li>
+                  <li className={navigationItemClass}>
+                    <NavLink to="/groups-and-events">Groups and events</NavLink>
+                  </li>
+                  <li className={navigationItemClass}>
+                    <NavLink to="/gallery">Gallery</NavLink>
+                  </li>
+                  <li className={navigationItemClass}>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-      <div className="language-swticher-container">
+        </nav>
+      </div>
+      <div className="language-swticher-container order-1 text-white ">
         <ul>
           <li>ENG</li>
         </ul>
