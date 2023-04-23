c2f0213b7cd4590ec14b8d2f60c5379843b5c580
Cuong Tran <mailtocuong@gmail.com>
2023-04-23 12:34:18 +0300

    remove not need file

--- app/services/markdown.server.ts
+++
@@ -1,5 +0,0 @@
-import { parse, transform, type RenderableTreeNodes } from "@markdoc/markdoc";
-
-export function markdown(markdown: string): RenderableTreeNodes {
-  return transform(parse(markdown));
-}
